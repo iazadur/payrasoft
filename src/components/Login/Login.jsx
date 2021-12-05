0061a6322f959338e7e73f22a1cf2fca1e6a155b
azadurzaagsyscom <azadur@zaagsys.com>
2021-12-05 17:13:43 +0600

    complete

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Form, Button, Card, ListGroup } from 'react-bootstrap'
+import { Form, Button, Card, ListGroup, Row, Col } from 'react-bootstrap'
 import swal from 'sweetalert';
 import Birthday from './Birthday';
 import Contact from './Contact';
@@ -42,7 +42,7 @@ const Login = () => {
             text: "You Registration Successfully!",
             icon: "success",
             button: false,
-          });
+        });
         const updateUser = localStorage.getItem('User')
         setUser(JSON.parse(updateUser))
 
@@ -58,13 +58,24 @@ const Login = () => {
                 {page === 2 && <Contact handleOnBlur={handleOnBlur} />}
                 {page === 3 && <Birthday handleOnBlur={handleOnBlur} />}
 
-                <div className="d-flex justify-content-between">
-                    <Button variant="warning" onClick={goPreviewsPage}>Previews</Button>
-                    <div className="">
-                        {page !== 3 && <Button variant="primary" className="" onClick={goNextPage}>Next</Button>}
-                        {page === 3 && <Button variant="success" type="submit">Submit</Button>}
-                    </div>
-                </div>
+
+
+                <Row className="mb-3" >
+                    <Col sm={2}>
+
+                    </Col>
+                    <Col sm={10}>
+                        <div className="d-flex justify-content-between">
+                            <Button variant="warning" onClick={goPreviewsPage}>Previews</Button>
+                            <div className="">
+                                {page !== 3 && <Button variant="primary" className="" onClick={goNextPage}>Next</Button>}
+                                {page === 3 && <Button variant="success" type="submit">Submit</Button>}
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+
+
 
 
             </Form>
